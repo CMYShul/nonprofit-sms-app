f9256251172ffd90c4757cfb6feab66ecaf48d7d
MosheRos <moshe4611@gmail.com>
2025-03-10 12:24:48 -0400

    Revert "Added Vercel Analytics"
    This reverts commit 268c135913f4fdc1236650b2f09aa7dc93806398.

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,22 +4,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 
-import { Analytics } from '@vercel/analytics/next';
- 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
